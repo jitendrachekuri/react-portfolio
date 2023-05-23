6afcb50c502fdcd0fccd37e4cbbdbb0dc2b419d5
jitendrachekuri <jitendra.chekuri@gmail.com>
2023-05-22 22:54:45 -0400

    initital commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,38 @@
+// work data
+
+export const Work = [
+    {
+        id: 1,
+        name: "Laurentian Bank",
+        role: "Full Stack Developer",
+        description: "Bulit and managed multiple Full stack applications for a Bank and handled a different teams for multiple projects.",
+        tags: ["React", "Angular", "NodeJS", "MongoDB"],
+        duration: "Jan 2022 - Present",
+        demo: "https://agency-website-eta.vercel.app/",
+        github: "https://github.com/codebucks27/Agency-website"
+    },
+    {
+        id: 2,
+        name: "One Local",
+        role: "Senior Software Engineer",
+        description: "Key role involvement in developing Web based applications For Logistics enterpise.",
+        tags: ["Angular", "NodeJS", "LoopBack", "MongoDB", "Bootstrap 4","Express JS"],
+        duration: "Dec 2020 - Jan 2022",
+        demo: "http://react-job-listing-website.codebucks27.vercel.app/",
+        github: "https://github.com/codebucks27/react-job-listing-website"
+
+    },
+    {
+        id: 3,
+        name: "Vzen Innovation Pvt Ltd",
+        role: "Software Engineer,",
+        description: "Played a key role in monitoring development activities and providing technical/process level guidance/support to the project team..",
+        tags: ["Angular", "NodeJS", "WebRTC", "Socket.io"],
+        duration: "Jan 2017 - July 2019",
+        demo: "http://devto-clone-codebucks27.vercel.app/",
+        github: "https://github.com/codebucks27/devto-clone"
+    },
+   
+
+
+] 
